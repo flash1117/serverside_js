d7fe725c935098d88d1fdbe1dc6544be2072986c
김기태 <flash1117@naver.com>
2020-07-24 22:08:01 +0900

    query string

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ let app = express();
 // 관습적으로 public 을 정적인 파일을 두는 위치로 삼는다.
 app.use(express.static("public")); // 정적인 파일이 위치할 디렉토리.
 
+app.get("/topic", (req, res) => {
+  let topics = ["Javascript", "Nodejs", "Express"];
+
+  let output2 = `
+    <a href="/topic?id=0">Javascript</a><br>
+    <a href="/topic?id=1">Nodejs</a><br>
+    <a href="/topic?id=2">Express</a><br><br>
+    ${topics[req.query.id]};
+  `;
+
+  res.send(output2);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello home page");
 }); // get 방식으로 접속
